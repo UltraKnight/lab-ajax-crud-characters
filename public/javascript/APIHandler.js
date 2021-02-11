1e8a640d96e969a1a08438733d6d641a4fee495e
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-11 18:47:16 +0000

    done somehow

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,66 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+   async getFullList () {
+    return await axios.get(this.BASE_URL + '/characters');
   }
 
-  getOneRegister () {
-
+ async getOneRegister (id) {
+    return await axios.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
+  async createOneRegister (character) {
+    let newChar = character;
+    if(!newChar.name) {
+      return newChar.name;
+    }
 
+    if (! newChar.occupation) {
+      return newChar.occupation;
+    }
+
+    if(! newChar.weapon) {
+      return newChar.weapon;
+    }
+
+    try {
+      let createdChar = await axios.post(this.BASE_URL + '/characters', newChar);
+      return createdChar;
+    } catch (error) {
+        return error;
+    }
   }
 
-  updateOneRegister () {
+  async updateOneRegister (id, character) {
+    let newChar = character;
+    if(! this.getOneRegister(id)) {
+      return 'Character not found';
+    }
 
+    let char = await this.getOneRegister(id);
+    char = char.data;
+    if(! newChar.name) {newChar.name = char.name;}
+    if(! newChar.occupation) {newChar.occupation = char.occupation;}
+    if(! newChar.weapon) {newChar.weapon = char.weapon;}
+
+    try {
+      let editedChar = await axios.put(`${this.BASE_URL}/characters/${id}`, newChar);
+      return editedChar;
+    } catch (error) {
+        return error;
+    }
   }
 
-  deleteOneRegister () {
+   async deleteOneRegister (id) {
+    if(! this.getOneRegister(id)) {
+      return 'Character not found';
+    }
 
+    try {
+      await axios.delete(`${this.BASE_URL}/characters/${id}`);
+      return 'Character has been successfully deleted';
+    } catch (error) {
+      return error;
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,81 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    let res = await charactersAPI.getFullList();
+    let data = res.data;
+    let charInfo = '';
+    data.forEach(char => {
+        charInfo += `
+        <div class="character-info">
+          <div class='id'>Id: ${char.id}</div>
+          <div class="name">Name: ${char.name}</div>
+          <div class="occupation">Occupation: ${char.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${char.cartoon ? 'Yes' : 'No'}</div>
+          <div class="weapon">Weapon: ${char.weapon}</div>
+        </div>`;
+    });
+    
+    document.querySelector('.characters-container').innerHTML = charInfo;
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    let id = document.querySelector('[name=character-id]').value;
+    let res = await charactersAPI.getOneRegister(id);
+    let char = res.data;
 
+    if(char.id) {
+      let charInfo = `
+      <div class="character-info">
+        <div class='id'>Id: ${char.id}</div>
+        <div class="name">Name: ${char.name}</div>
+        <div class="occupation">Occupation: ${char.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${char.cartoon ? 'Yes' : 'No'}</div>
+        <div class="weapon">Weapon: ${char.weapon}</div>
+      </div>`;
+    
+      document.querySelector('.characters-container').innerHTML = charInfo;
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    let id = document.querySelector('[name=character-id-delete]').value;
+    let res = await charactersAPI.deleteOneRegister(id);
+    if(res === 'Character has been successfully deleted') {
+      event.target.style.backgroundColor = 'green';
+      document.getElementById('fetch-all').click(); //simulate a click on the fetch-all button
+    } else {
+      event.target.style.backgroundColor = 'red';
+    }
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    let id = event.target.elements['chr-id'].value;
+    let name = event.target.elements.name.value;
+    let occupation = event.target.elements.occupation.value;
+    let weapon = event.target.elements.weapon.value;
+    let cartoon = event.target.elements.cartoon.checked;
+    let char = {name, occupation, weapon, cartoon};
 
+    try {
+      await charactersAPI.updateOneRegister(id, char);
+      event.target.elements['send-data'].style.backgroundColor = 'green';
+      document.getElementById('fetch-all').click(); //simulate a click on the fetch-all button
+    } catch (error) {
+      event.target.elements['send-data'].style.backgroundColor = 'red';
+    }
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    let name = event.target.elements.name.value;
+    let occupation = event.target.elements.occupation.value;
+    let weapon = event.target.elements.weapon.value;
+    let cartoon = event.target.elements.cartoon.checked;
+    let char = {name, occupation, weapon, cartoon};
 
+    await charactersAPI.createOneRegister(char);
+    document.getElementById('fetch-all').click(); //simulate a click on the fetch-all button
   });
 });
